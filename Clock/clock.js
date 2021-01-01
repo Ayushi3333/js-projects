376580c343414492ea52c7f956249ddb8c37cf45
ayushi gupta <ayushi3101gupta@gmail.com>
2021-01-01 17:16:48 +0100

    Refactor code

--- Clock/clock.js
+++ Clock/clock.js
@@ -5,22 +5,19 @@ const hourHand = document.querySelector(".hour");
 setDate = () => {
 const dateToday = new Date();
 
+// Add seconds Hand
 const sec = dateToday.getSeconds()
-
 const secDegrees = (sec / 60) * 360 + 90;
-
 secHand.style.transform = `rotate(${secDegrees}deg)`;
 
-
+// Add minutes Hand
 const mins = dateToday.getMinutes()
 const minDegrees = (mins/60) * 360 + (sec/60) * 6 + 90;
-console.log(minsHand)
 minsHand.style.transform = `rotate(${minDegrees}deg)`;
 
-
+// Add Hour Hand
 const hour = dateToday.getHours() % 12 || 12
 const hourDegrees = (hour / 12) * 360 + (mins/60)*30 + 90;
-console.log(hourHand)
 hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
 };
